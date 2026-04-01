4081c89ce19ae73508629208bae8f8c403585898
CafeHable <jopresent1202@gmail.com>
2026-04-01 20:55:35 +0900

    로그인 실패 테스트 추가

--- tests/naver.spec.js
+++ tests/naver.spec.js
@@ -7,3 +7,11 @@ test('로그인 성공 테스트', async ({ page }) => {
   await page.click('#submit');
   await expect(page.locator('.post-title')).toHaveText('Logged In Successfully');
 });
+
+test('로그인 실패 테스트', async ({ page }) => {
+  await page.goto('https://practicetestautomation.com/practice-test-login/');
+  await page.fill('#username', 'wronguser');
+  await page.fill('#password', 'wrongpassword');
+  await page.click('#submit');
+  await expect(page.locator('#error')).toBeVisible();
+});
